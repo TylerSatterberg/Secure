a3d88413b4d93d14f637634d88679eff3bb416b8
TylerSatterberg <satterbergtyler@gmail.com>
2024-11-13 13:02:19 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -17,23 +17,23 @@ app.secret_key=os.environ["SECRET_KEY"]; #This is an environment variable.
 def renderMain():
     return render_template('home.html')
 
-@app.route('/startOver')
-def startOver():
+@app.route('/tryagain')
+def tryagain():
     session.clear() #clears variable values and creates a new session
     return redirect(url_for('renderMain')) # url_for('renderMain') could be replaced with '/'
 
-@app.route('/page1')
-def renderPage1():
-    return render_template('page1.html')
+@app.route('/p1')
+def renderP1():
+    return render_template('p1.html')
 
-@app.route('/page2',methods=['GET','POST'])
-def renderPage2():
+@app.route('/p2',methods=['GET','POST'])
+def renderP2():
     session["firstName"]=request.form['firstName']
     session["lastName"]=request.form['lastName']
     return render_template('page2.html')
 
-@app.route('/page3',methods=['GET','POST'])
-def renderPage3():
+@app.route('/p3',methods=['GET','POST'])
+def renderP3():
     session["favoriteColor"]=request.form['favoriteColor']
     return render_template('page3.html')
     
