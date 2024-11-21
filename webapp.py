8bfddf93a933bba94da8c14b8daac63ea81e79e3
TylerSatterberg <satterbergtyler@gmail.com>
2024-11-21 13:12:03 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, url_for, render_template, request
 from flask import redirect
 from flask import session
-
+import time
 app = Flask(__name__)
 
 # In order to use "sessions",you need a "secret key".
@@ -23,19 +23,55 @@ def tryagain():
     return redirect(url_for('renderMain')) # url_for('renderMain') could be replaced with '/'
 
 @app.route('/p1')
-def renderP1():
+def renderp1():
+    session["start_time"] = time.time()
     return render_template('p1.html')
 
 @app.route('/p2',methods=['GET','POST'])
-def renderP2():
-    session["firstName"]=request.form['firstName']
-    session["lastName"]=request.form['lastName']
-    return render_template('page2.html')
+def renderp2():
+    session["Equilibrium"]=request.form['Equilibrium']
+    return render_template('p2.html')
 
 @app.route('/p3',methods=['GET','POST'])
-def renderP3():
-    session["favoriteColor"]=request.form['favoriteColor']
-    return render_template('page3.html')
+def renderp3():
+    session["avacado"]=request.form['avacado']
+    return render_template('p3.html')
+
+@app.route('/p4',methods=['GET','POST'])
+def renderp4():
+    session["sigfig"]=request.form['sigfig']
+    return render_template('p4.html')
     
+@app.route("/p5", methods=['GET', 'POST'])
+def renderp5():
+    session["end_time"] = time.time()
+    T = session["end_time"] - session["start_time"]
+    session["Ksp"]=request.form['Ksp']
+    t=0
+    if session["Equilibrium"] == "A physical change occurs, and solvation is exothermic":
+        t = t + 1 
+    else:
+        t = t
+        
+    if session["avacado"] == "6.022x10^23":
+        t = t + 1 
+    elif session["avacado"] == "6.02x10^23":
+        t = t + 0.5
+    else:
+        t = t
+        
+    if session["sigfig"] == "6 figures":
+        t = t + 1 
+    else:
+        t = t
+        
+    if session["Ksp"] == "3.0x10^-4":
+        t = t + 1 
+        print("t")
+    else:
+        t = t
+    Time = round(T, 0)
+    return render_template('p5.html', total=t, Time=Time)
+
 if __name__=="__main__":
-    app.run(debug=False)
+    app.run(debug=True)
